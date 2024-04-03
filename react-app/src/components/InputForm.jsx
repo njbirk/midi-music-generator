409392a2762ad47f43d3cac207998c2bc4436470
Nolan Birkeland <nolan.birkeland@gmail.com>
2024-04-03 00:38:39 -0400

    all dropdowns in place

--- react-app/src/components/InputForm.jsx
+++ react-app/src/components/InputForm.jsx
@@ -7,7 +7,7 @@ import {
   Button,
   TextInput,
 } from "grommet";
-import Dropdown from './dropdown'
+import Dropdown from "./dropdown";
 
 const InputForm = () => {
   const [songName, setSongName] = React.useState("");
@@ -39,49 +39,82 @@ const InputForm = () => {
       .catch((error) => console.error(error));
   }
 
-
   const dropdownData = {
-    dropdownOne: [
-      { label: 'Option 1', value: 'option1' },
-      { label: 'Option 2', value: 'option2' },
+    Speed: [
+      { label: "fast", value: "fast" },
+      { label: "medium", value: "medium" },
+      { label: "slow", value: "slow" },
     ],
-    dropdownTwo: [
-      { label: 'Option A', value: 'optionA' },
-      { label: 'Option B', value: 'optionB' },
+    Tone: [
+      { label: "happy", value: "happy" },
+      { label: "sad", value: "sad" },
+    ],
+    Volume: [
+      { label: "loud", value: "loud" },
+      { label: "medium", value: "medium" },
+      { label: "quiet", value: "quiet" },
+    ],
+    Instrument: [
+      { label: "piano", value: "piano" },
+      { label: "electric piano", value: "electric piano" },
+    ],
+    Repeatability: [
+      { label: "1", value: "1" },
+      { label: "2", value: "2" },
+      { label: "3", value: "3" },
+      { label: "4", value: "4" },
+      { label: "5", value: "5" },
+      { label: "6", value: "6" },
+      { label: "7", value: "7" },
+      { label: "8", value: "8" },
+      { label: "9", value: "9" },
+      { label: "10", value: "10" },
+    ],
+    Structuredness: [
+      { label: "1", value: "1" },
+      { label: "2", value: "2" },
+      { label: "3", value: "3" },
+      { label: "4", value: "4" },
+      { label: "5", value: "5" },
+      { label: "6", value: "6" },
+      { label: "7", value: "7" },
+      { label: "8", value: "8" },
+      { label: "9", value: "9" },
+      { label: "10", value: "10" },
     ],
   };
 
   const [selected, setSelected] = React.useState({});
 
-
   const handleChange = (name) => (event) => {
-    setSelected(prevSelected => ({
+    setSelected((prevSelected) => ({
       ...prevSelected,
       [name]: event.target.value,
     }));
   };
-
 
   return (
     <Card height="medium" width="medium" background="dark-3">
       <CardHeader margin="xsmall" alignSelf="center">
         <Heading level="3">Create Your First Song!</Heading>
       </CardHeader>
-      <CardBody margin="small">
+      <CardBody margin="small" overflow="auto">
         <TextInput
           placeholder="Enter a song name..."
           value={songName}
           onChange={updateSongName}
         />
         <>
-        {Object.entries(dropdownData).map(([name, options]) => (
-        <div key={name}>
-          <Dropdown
-            name={name}
-            options={options}
-            onChange={handleChange(name)}
-          />
-        </div>))}
+          {Object.entries(dropdownData).map(([name, options]) => (
+            <div key={name}>
+              <Dropdown
+                title={name}
+                name={name}
+                options={options}
+                onChange={handleChange(name)}
+              />
+            </div>
+          ))}
         </>
         <Button primary margin="small" label="Go!" onClick={API_call} />
       </CardBody>
